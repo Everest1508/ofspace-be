384f46223ceb738ae864fb273cf7ccc9618609a8
Everest1508 <riteshmahale15@gmail.com>
2025-12-30 22:19:44 +0530

    cc

--- ofspace/settings.py
+++ ofspace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ynpvvojy(ddkf^fx4msmt84!ast4%hnf)mo4txz@&=n_t74)0b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
